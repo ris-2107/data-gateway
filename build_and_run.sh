f1205fa77fd517295ae4824fd370e93cce08989c
ris2107 <rishabh210798@gmail.com>
2024-04-06 12:55:28 +0530

    init

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,38 +3,36 @@
 # Set execute permissions for the script
 chmod +x "$0"
 
-
 echo "Starting build and run script..."
 
-echo "Pulling the latest changes from the Git repository...\n"
+echo "Pulling the latest changes from the Git repository..."
 git pull origin master
 
-echo "Please enter the port number and service name when prompted.\n"
+echo "Please enter the port number and service name when prompted."
 read -p "Enter port number: " port
 read -p "Enter service name: " service_name
 
-echo "\nStopping the existing container..."
+echo -e "\nStopping the existing container..."
 docker stop $service_name
 
-echo "\nRemoving the existing container..."
+echo -e "\nRemoving the existing container..."
 docker rm $service_name >/dev/null 2>&1
 
 # Upgrade Gradle wrapper
-echo "Upgrading Gradle wrapper to version 7.3...\n"
+echo "Upgrading Gradle wrapper to version 7.3..."
 ./gradlew wrapper --gradle-version 7.3
 
-echo "\nBuilding the project with Gradle...\n"
+echo -e "\nBuilding the project with Gradle..."
 ./gradlew build
 
-# shellcheck disable=SC2028
-echo "Building Docker image...\n"
+echo -e "Building Docker image...\n"
 docker build -t $service_name-img .
 
-echo "\nStarting the container with the following settings:"
+echo -e "\nStarting the container with the following settings:"
 echo "  Port: $port"
 echo "  Service name: $service_name"
 docker run -p $port:$port --name $service_name -d $service_name-img >/dev/null 2>&1
 
 # Stream container logs
-echo "Streaming logs for container $service_name. Press Ctrl+C to exit.\n"
+echo -e "Streaming logs for container $service_name. Press Ctrl+C to exit.\n"
 docker logs -f $service_name
